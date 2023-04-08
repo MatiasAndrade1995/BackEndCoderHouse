f95e65fc0a3ec9d30229101719e5e2512995df42
Matias Andrade <matiasandrade.unla@gmail.com>
2023-04-08 16:58:04 -0300

    correcciones Desafio 1

--- desafioentregable1.js
+++ desafioentregable1.js
@@ -1,47 +1,59 @@
 //Desafio Entregable 1 - Andrade Matias
 
 class ProductManager {
-    #name
+
+    #products
 
     constructor() {
-        this.products = []
+        this.#products = []
     }
 
-    addProduct = (title, description, price, thumbail, code, stock,) => {
+    addProduct = (title, description, price, thumbnail, code, stock,) => {
 
-        if (title === undefined || description === undefined || price === undefined || thumbail === undefined || code === undefined || stock === undefined) {
-            console.log("Todos los campos son requeridos")
+        if (title === undefined || description === undefined || price === undefined || thumbnail === undefined || code === undefined || stock === undefined) {
+            console.log("All fields are required")
             return;
         }
 
-        const objectWithCode = this.products.find(element => element.code == code)
+        const objectWithCode = this.#products.find(element => element.code == code)
         if (objectWithCode) {
-            console.log("ERROR... Este producto con ese código ya existe")
+            console.log(`ERROR... The product with this Code: ${code} already exists`)
 
         } else {
 
-            const id = this.products.length == 0 ? 1 : this.products[this.products.length - 1].id + 1
+            const id = this.#products.length == 0 ? 1 : this.#products[this.#products.length - 1].id + 1
 
-            this.products.push({
+            this.#products.push({
                 id,
                 title,
                 description,
                 price,
-                thumbail,
+                thumbnail,
                 code,
                 stock
-
             })
+
         }
     }
 
-    getProducts = () => this.products;
+    updateProduct = (id, modify) => {
+        let indexProduct = this.#products.findIndex(el => el.id == id)
+        const objectModified = this.#products[indexProduct] = { ...this.#products[indexProduct], ...modify }
+        console.log(objectModified)
+
+    };
+
+    getProducts = () => {
+        console.log(this.#products);
+        return this.#products;
+    }
 
     getProductById = (id) => {
-        const objectWithId = this.products.find(obj => obj.id == id)
+        const objectWithId = this.#products.find(obj => obj.id == id)
 
         if (objectWithId) {
 
+            console.log(objectWithId)
             return objectWithId;
 
         } else {
@@ -54,18 +66,12 @@ class ProductManager {
 
 //Proceso Testing 
 
-let productoPrueba1 = new ProductManager();
+let productManager = new ProductManager();
 
-console.log(productoPrueba1.getProducts()); //Trae array vacío ya que todavia no hay producto agregado.
+productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin Imagen", "abc123", 0) // Ejecuto metodo addProduct de la clase para agregar producto.
 
-productoPrueba1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin Imagen", "abc123", 0) // Ejecuto metodo addProduct de la clase para agregar producto.
+productManager.getProductById(1)
 
-productoPrueba1.addProduct("producto prueba", "Este es un producto prueba", "Sin Imagen", "abc123", 25) // Ejecuto metodo addProduct de la clase para agregar producto. Este al ser enviado sin el price, crea un mensaje por consola que indica que todos los campos son requeridos para ser agregado.
+productManager.updateProduct(1, { title: "producto prueba2", stock: 2 })
 
-console.log(productoPrueba1.getProducts());
-
-productoPrueba1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin Imagen", "abc123", 25) // Ejecuto nuevamente el addProduct de la clase para agregar producto. Sin embargo, este no se agrega porque contiene el mismo código.
-
-console.log(productoPrueba1.getProductById(1)) // Como el producto con el ID 1 existe, me retorna el producto.
-
-productoPrueba1.getProductById(2) // Como el producto con el ID 2 no existe, me devuelve en la consola Not Found.
+productManager.getProductById(1)
